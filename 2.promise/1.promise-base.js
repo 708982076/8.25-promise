747629730cec48842c7cc21ad645398b80377783
Your Name <you@example.com>
2018-08-25 11:42:40 +0800

    promise then

--- 2.promise/1.promise-base.js
+++ 2.promise/1.promise-base.js
@@ -6,41 +6,41 @@ function Promise(executor) {
   self.onResolved = []; // 专门存放成功的回调
   self.onRejected = []; // 专门存放失败的回调
   function resolve(value) {
-    if(self.status === 'pending'){
+    if (self.status === 'pending') {
       // 将成功的原因保留起来
-      self.value = value; 
+      self.value = value;
       self.status = 'resolved';
       self.onResolved.forEach(fn => fn());
     }
   }
   function reject(reason) {
-    if (self.status === 'pending'){
+    if (self.status === 'pending') {
       self.reason = reason;
       self.status = 'rejected';
       self.onRejected.forEach(fn => fn());
     }
   }
-  try{
+  try {
     executor(resolve, reject);
-  }catch(e){
+  } catch (e) {
     reject(e);
   }
 }
-Promise.prototype.then = function (onfulfilled,onrejected) {
+Promise.prototype.then = function (onfulfilled, onrejected) {
   let self = this;
-  if (self.status === 'resolved'){
+  if (self.status === 'resolved') {
     onfulfilled(self.value);
   }
   if (self.status === 'rejected') {
     onrejected(self.reason);
   }
-  if(self.status === 'pending'){
-    self.onResolved.push(function(){
+  if (self.status === 'pending') {
+    self.onResolved.push(function () {
       onfulfilled(self.value);
     });
     self.onRejected.push(function () {
       onrejected(self.reason);
-    })
+    });
   }
 }
 module.exports = Promise
